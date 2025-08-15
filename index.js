4a68f31f54c5c152a8571b19e2b4bb52a2d83cb0
aparn-gupta <aparnagupta813@gmail.com>
2025-08-16 04:01:12 +0530

    updates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mysql = require("mysql2/promise");
 const cors = require("cors");
+const jwt = require("jsonwebtoken")
 
 dotenv.config();
 
@@ -36,13 +37,12 @@ pool.getConnection((err, conn) => {
   }
 });
 
-let allComparisons = []
-
+let allComparisons = [];
 
 const faceMatcher = (desOne, desTwo, userName, userId) => {
   // console.log(desOne)
   // console.log(desTwo)
-  const desOneParsed =  Array.isArray(desOne) ? desOne : JSON.parse(desOne);
+  const desOneParsed = Array.isArray(desOne) ? desOne : JSON.parse(desOne);
 
   let differenceOfSquares = [];
 
@@ -67,14 +67,11 @@ const faceMatcher = (desOne, desTwo, userName, userId) => {
 
   // console.log(euclideanDistance, username);
   allComparisons.push({
-      distanceIndicator: euclideanDistance,
-      personName: userName,
-      personId: userId
-
-  })
-
+    distanceIndicator: euclideanDistance,
+    personName: userName,
+    personId: userId,
+  });
 };
-
 
 const faceMatcherForLogin = (desOne, desTwo) => {
   const desOneParsed = Array.isArray(desOne) ? desOne : JSON.parse(desOne);
@@ -101,28 +98,22 @@ const faceMatcherForLogin = (desOne, desTwo) => {
   const euclideanDistance = Math.sqrt(sumOfDifferences);
 
   // console.log(euclideanDistance, username);
- if (euclideanDistance > 0.6) {
-  return false 
- } else {
-  return true
- }
-
+  if (euclideanDistance > 0.6) {
+    return false;
+  } else {
+    return true;
+  }
 };
 
-
 const findIfNoMatches = (allFacesArrays) => {
-  for (let y = 0 ; y < allFacesArrays.length; y++) {
-    if  (allFacesArrays[y].distanceIndicator  < 0.6) {
-      return false
+  for (let y = 0; y < allFacesArrays.length; y++) {
+    if (allFacesArrays[y].distanceIndicator < 0.6) {
+      return false;
     }
-
   }
 
-  return true
- }
-
-
-
+  return true;
+};
 
 app.post("/sendfacedata", async (req, res) => {
   //    if (!req.body || !req.body.empName || !req.body.descriptorArray) {
@@ -130,32 +121,32 @@ app.post("/sendfacedata", async (req, res) => {
   //         error: "must contain username and descriptor array"
   //     })
 
-  allComparisons = []
+  allComparisons = [];
 
   const { empName, descriptorArray } = req.body;
 
+  let currentUserFaceParsed = Array.isArray(descriptorArray)
+    ? descriptorArray
+    : Array.from(JSON.parse(descriptorArray));
 
-  let currentUserFaceParsed = Array.isArray(descriptorArray) ? descriptorArray : Array.from(JSON.parse(descriptorArray)) ;
-  
   const [rows] = await pool.query("SELECT * FROM facedata");
 
   // console.log(empName, descriptorArray);
 
-  console.log(currentUserFaceParsed)
-
-
-
-
-
+  console.log(currentUserFaceParsed);
 
   for (let each of rows) {
     if (each.descriptorArr) {
-      faceMatcher(each.descriptorArr, currentUserFaceParsed, each.username, each.id);
-
+      faceMatcher(
+        each.descriptorArr,
+        currentUserFaceParsed,
+        each.username,
+        each.id
+      );
     }
   }
 
-  const matchResult = findIfNoMatches(allComparisons)
+  const matchResult = findIfNoMatches(allComparisons);
 
   // let dataInArr = []
   // for (let each of currentUserFaceParsed) {
@@ -164,10 +155,9 @@ app.post("/sendfacedata", async (req, res) => {
 
   if (!matchResult) {
     res.status(200).json({
-      message: "User already registred!"
-    })
+      message: "User already registred!",
+    });
   } else {
-
     try {
       const [rows] = await pool.query(
         "INSERT INTO facedata (username,  descriptorArr) VALUES (?, ?)",
@@ -181,19 +171,9 @@ app.post("/sendfacedata", async (req, res) => {
       console.log(err);
       res.status(500).json({ error: "error processing request" });
     }
-
   }
 
-
-
-
-
-
-
-
   // const {descriptorArray} = req.body
-
-
 });
 
 app.get("/hello", (req, res) => {
@@ -202,186 +182,81 @@ app.get("/hello", (req, res) => {
 
 
 
-// app.post("/compare", async (req, res) => {
-//   allComparisons = []
-
-//   let { userFace } = req.body;
-//   const currentUserFaceParsed = JSON.parse(userFace);
-//   const [rows] = await pool.query("SELECT * FROM facedata");
-
-
-
-
-
-//   for (let each of rows) {
-//     faceMatcher(each.descriptorArr, currentUserFaceParsed, each.username, each.id);
-//   }
-
-
-//   console.log(allComparisons)
-
-//   let bestMatch = allComparisons[0].distanceIndicator
-//   let bestMatchUserName = allComparisons[0].personName
-//   let bestMatchUserId = allComparisons[0].personId
-
-
-
-//  const matchResult = findIfNoMatches(allComparisons)
-
-
-//  if (matchResult) {
-//   res.json({
-//     success: true,
-//     bestMatchFace: "No matching faces found",
-//     indicator: null
-//   });
-
-
-//  } else {
-//   for (let y = 0 ; y < allComparisons.length; y++) {
-//     console.log(allComparisons[y].distanceIndicator)
-//     if (allComparisons[y].distanceIndicator < bestMatch) {
-//         bestMatch = allComparisons[y].distanceIndicator
-//         bestMatchUserName = allComparisons[y].personName
-//         bestMatchUserId = allComparisons[y].personId
-//         console.log(allComparisons[y].personName, bestMatchUserName)
-//     }
-// }
-
-// res.json({
-//   success: true,
-//   bestMatchFace: bestMatchUserName,
-//   id: bestMatchUserId,
-//   indicator: bestMatch
-// });
-
-
-//  }
-
-  
-
-
-
-
-
-
-// });
-
-
 
 app.post("/loginface", async (req, res) => {
-
-
-  if (!req.body.userFace ) {
+  if (!req.body.userFace) {
     res.status(501).json({
-      error: "Face array is required!"
-    })
+      error: "Face array is required!",
+    });
 
-    return
-
-  } 
-
-  
+    return;
+  }
 
   let { userFace } = req.body;
 
- 
+  allComparisons = [];
 
+  let currentUserFaceParsed = Array.isArray(userFace)
+    ? userFace
+    : Array.from(JSON.parse(userFace));
 
-  allComparisons = []
-
-
-
-  let currentUserFaceParsed = Array.isArray(userFace) ? userFace : Array.from(JSON.parse(userFace)) ;
-  
   const [rows] = await pool.query("SELECT * FROM facedata");
 
   // console.log(empName, descriptorArray);
 
   // console.log(currentUserFaceParsed)
 
-
-
-
-
-
   for (let each of rows) {
     if (each.descriptorArr) {
-      faceMatcher(each.descriptorArr, currentUserFaceParsed, each.username, each.id);
-
+      faceMatcher(
+        each.descriptorArr,
+        currentUserFaceParsed,
+        each.username,
+        each.id
+      );
     }
   }
 
+  // console.log(allComparisons);
 
-  console.log(allComparisons)
+  let bestMatch = allComparisons[0].distanceIndicator;
+  let bestMatchUserName = allComparisons[0].personName;
+  let bestMatchUserId = allComparisons[0].personId;
 
+  const matchResult = findIfNoMatches(allComparisons);
 
-
-
-
-    let bestMatch = allComparisons[0].distanceIndicator
-  let bestMatchUserName = allComparisons[0].personName
-  let bestMatchUserId = allComparisons[0].personId
-
-
-  const matchResult = findIfNoMatches(allComparisons)
-
-
-
- if (matchResult) {
-  res.json({
-    success: true,
-    bestMatchFace: "No matching faces found",
-    indicator: null
-  });
-
-
- } else {
-  for (let y = 0 ; y < allComparisons.length; y++) {
-    console.log(allComparisons[y].distanceIndicator)
-    if (allComparisons[y].distanceIndicator < bestMatch) {
-        bestMatch = allComparisons[y].distanceIndicator
-        bestMatchUserName = allComparisons[y].personName
-        bestMatchUserId = allComparisons[y].personId
-        console.log(allComparisons[y].personName, bestMatchUserName)
+  if (matchResult) {
+    res.json({
+      success: true,
+      bestMatchFace: "No matching faces found",
+      indicator: null,
+    });
+  } else {
+    for (let y = 0; y < allComparisons.length; y++) {
+      console.log(allComparisons[y].distanceIndicator);
+      if (allComparisons[y].distanceIndicator < bestMatch) {
+        bestMatch = allComparisons[y].distanceIndicator;
+        bestMatchUserName = allComparisons[y].personName;
+        bestMatchUserId = allComparisons[y].personId;
+        console.log(allComparisons[y].personName, bestMatchUserName);
+      }
     }
-}
 
-console.log(bestMatchUserName)
+    // console.log(bestMatchUserName);
 
-res.json({
-  success: true,
-  bestMatchFace: bestMatchUserName,
-  id: bestMatchUserId,
-  indicator: bestMatch
+   const loginToken =  jwt.sign({userId: bestMatchUserId, bestMatchFace: bestMatchUserName}, process.env.JWT_SECRET)
 
+    console.log(loginToken)
+
+    res.json({
+      success: true,
+      bestMatchFace: bestMatchUserName,
+      id: bestMatchUserId,
+      indicator: bestMatch,
+      loginToken: loginToken
+    });
+  }
 });
-
-
-
- }
-
-
-
-
-
-
- 
-
-
-
-
-  
-
-
-
-
-
-
-});
-
-
-
 
 app.listen(PORT, () => {
   console.log("App listening to port " + PORT);
